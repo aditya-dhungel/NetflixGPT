53c822691f483d86ce6d24c795f44a3e4c57290f
Aditya Dhungel <adityadhungel123@gmail.com>
2026-01-27 23:14:04 +0530

    Resolve merge conflicts and unify Redux + Tailwind setup

--- src/App.js
+++ src/App.js
@@ -1,3 +1,27 @@
+// <<<<<<< HEAD
+// import Body from "./components/Body";
+// import appStore from "./utils/appStore";
+// import { Provider } from "react-redux";
+
+// function App() {
+//   return (
+//     <Provider store={appStore}>
+//       <Body />
+//     </Provider>
+// =======
+// import "./App.css";
+
+// function App() {
+//   return (
+//     <div className="font-4xl font-serif">
+//       NetflixGpt
+//     </div>
+// >>>>>>> 9fcdac7 (cra and tailwind)
+//   );
+// }
+
+// export default App;
+
 import Body from "./components/Body";
 import appStore from "./utils/appStore";
 import { Provider } from "react-redux";
@@ -11,3 +35,4 @@ function App() {
 }
 
 export default App;
+
